67945f5ac981642e7407920a4d3a05b6f9679869
GitHub1503 <gavrilov1979@gmail.com>
2023-08-24 14:05:06 +0300

    New file 17_7_3 added

--- 17_7_3.py
+++ 17_7_3.py
@@ -1,7 +1,6 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = 100000
+money = 187349
 deposit = []
 for key in per_cent.keys():
     deposit.append(int(money/100 * per_cent[key]))
-print(f'deposit = {deposit}')
 print(f'Максимальная сумма, которую вы можете заработать — {max(deposit)}')
