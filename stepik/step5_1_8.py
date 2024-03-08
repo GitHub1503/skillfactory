34b7d9f35e225bf68323448665b6d2ba72b00357
GitHub1503 <gavrilov1979@gmail.com>
2024-03-08 18:09:37 +0300

    New file added step5_1_8

---
+++ stepik/step5_1_8.py
@@ -0,0 +1,3 @@
+x1, y1, x2, y2 = int(input('Введите число: ')), int(input('Введите число: ')), int(input('Введите число: ')), int(input('Введите число: '))
+if (abs(x1-x2) == abs(y1-y2)) or x1 == x2 or y1 == y2:
+    print('YES')
