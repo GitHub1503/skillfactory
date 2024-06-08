c40b90d0ef3599302f8756c70a56762b3d87139b
GitHub1503 <gavrilov1979@gmail.com>
2024-06-08 16:39:51 +0300

    file 26_2_a added

---
+++ 26_2/26_2_a.py
@@ -0,0 +1,14 @@
+def my_decorator(func):
+    def wrapper():
+        print("Начало выполнения функции.")
+        func()
+        print("Конец выполнения функции.")
+
+    return wrapper
+
+#Эту функцию мы будем декорировать
+def my_first_decorator():
+	print("Это мой первый декоратор!")
+
+my_first_decorator = my_decorator(my_first_decorator)
+my_first_decorator()
